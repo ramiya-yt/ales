7a1abe4c6933db7871b970918cb2e7abe37a10c4
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-09-10 18:46:38 +0530

    Update testmenu.js

--- plugins/testmenu.js
+++ plugins/testmenu.js
@@ -4,8 +4,8 @@ const Config = require('../config');
 const ldc = "ssf"
 
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
-        let regex1 = new RegExp('*SINHALA*')
-        let regex2 = new RegExp('*ENGLISH*')
+        let regex1 = new RegExp('SINHALA')
+        let regex2 = new RegExp('ENGLISH')
 
         if (regex1.test(message.message)) {             
             await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
