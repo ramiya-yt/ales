1ae18cd98a3bc8b036c5f9c3de8b34f976d22a31
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-09-10 19:07:03 +0530

    Create menut1.js

---
+++ plugins/menut1.js
@@ -0,0 +1,16 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const ldc = "ssf"
+
+Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+        let regex1 = new RegExp('tmenu')
+        let regex2 = new RegExp('english')
+        
+        if (regex1.test(message.message)) {             
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        } 	
+        else if (regex2.test(message.message)) {       
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        }
+}));
