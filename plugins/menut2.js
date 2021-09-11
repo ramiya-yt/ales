baa03b68fd468a696961719a5b2e1f2fcdc51f52
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2021-09-11 13:44:10 +0530

    Update menut2.js

--- plugins/menut2.js
+++ plugins/menut2.js
@@ -4,9 +4,9 @@ const Config = require('../config');
 const ldc = "ssf"
 
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
-        let regex1 = new RegExp('tmna')
-        let regex2 = new RegExp('⋮☰ 𝐒𝐈𝐍𝐇𝐀𝐋𝐀 𝐌𝐄𝐍𝐔')
-        let regex3 = new RegExp('⋮☰ 𝐄𝐍𝐆𝐋𝐈𝐒𝐇 𝐌𝐄𝐍𝐔')
+        let regex1 = new RegExp("tmna")
+        let regex2 = new RegExp("⋮☰ 𝐒𝐈𝐍𝐇𝐀𝐋𝐀 𝐌𝐄𝐍𝐔")
+        let regex3 = new RegExp("⋮☰ 𝐄𝐍𝐆𝐋𝐈𝐒𝐇 𝐌𝐄𝐍𝐔")
 
         if (regex1.test(message.message)) {             
             await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
